1e13fc393e80ef11ecd94700e722a1df01893f2a
Sandeep-0404 <sandeepgh2002@gmail.com>
2021-06-27 23:39:13 +0530

    fetch

--- app/src/main/java/com/example/upsczindabaad/recviewmainactivity_k.java
+++ app/src/main/java/com/example/upsczindabaad/recviewmainactivity_k.java
@@ -1,5 +1,6 @@
 package com.example.upsczindabaad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,8 +10,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -19,6 +25,9 @@ RecyclerView rvk;
 adapterk adapter;
 EditText etks1,etks2;
 ArrayList<modelk>mak;
+FirebaseUser user;
+    FirebaseDatabase db;
+    DatabaseReference ref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,17 +38,39 @@ ArrayList<modelk>mak;
         modelk md=new modelk();
 etks1=(EditText)findViewById(R.id.etk1);
 etks2=(EditText)findViewById(R.id.etk2);
+user=FirebaseAuth.getInstance().getCurrentUser();
+
+         db=FirebaseDatabase.getInstance();
+        ref=db.getReference("saurav_sandeep");
 
         adapter=new adapterk(mak);
         rvk.setAdapter(adapter);
+
+        fetchdata();
+    }
+
+    private void fetchdata() {
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
     }
 
     public void send(View view) {
         String msg=etks1.getText().toString().trim();
         String name=etks2.getText().toString().trim();
-        String id="1";
-        FirebaseDatabase db=FirebaseDatabase.getInstance();
-        DatabaseReference ref=db.getReference("uid");
+        String id=user.getUid();
+
         dataholder obj=new dataholder(msg,name,id);
         ref.setValue(obj);
         etks1.setText("");
