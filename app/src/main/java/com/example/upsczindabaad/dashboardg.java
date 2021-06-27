beaf4310379c970757f1f813bb8313e530572a0b
Sandeep-0404 <sandeepgh2002@gmail.com>
2021-06-27 13:42:20 +0530

    Login page with function(SG)

--- app/src/main/java/com/example/upsczindabaad/dashboardg.java
+++ app/src/main/java/com/example/upsczindabaad/dashboardg.java
@@ -3,15 +3,12 @@ package com.example.upsczindabaad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    TextView textView;
+public class dashboardg extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dashboardg);
     }
 }
