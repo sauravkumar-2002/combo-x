c4f49898f0c59fd5aa14353e7d36c5fe0fac5f23
Saurav Kumar <https://github.com/sauravkumar-2002/Health-Companion>
2021-06-27 15:46:38 +0530

    layout sk4

--- app/src/main/java/com/example/upsczindabaad/dashboardg.java
+++ app/src/main/java/com/example/upsczindabaad/dashboardg.java
@@ -3,6 +3,7 @@ package com.example.upsczindabaad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class dashboardg extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class dashboardg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboardg);
+    }
+
+    public void createroom(View view) {
+    }
+
+    public void joinroom(View view) {
+    }
+
+    public void startchat(View view) {
     }
 }
