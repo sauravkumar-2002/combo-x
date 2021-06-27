aea8a3f0005066f80383cfc9f0993c7503efac8d
Sandeep-0404 <sandeepgh2002@gmail.com>
2021-06-27 14:31:42 +0530

    trial

---
+++ app/src/main/java/com/example/upsczindabaad/signUpactivity.java
@@ -0,0 +1,21 @@
+package com.example.upsczindabaad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class signUpactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_upactivity);
+    }
+
+    public void signUp(View view) {
+    }
+
+    public void LogIN(View view) {
+    }
+}
