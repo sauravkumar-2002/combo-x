bd6f0973ff963fe3653570638ec4dcea164454e5
Sandeep-0404 <sandeepgh2002@gmail.com>
2021-07-24 17:39:00 +0530

    dashboard updated

--- app/src/main/java/com/example/upsczindabaad/dashboardg.java
+++ app/src/main/java/com/example/upsczindabaad/dashboardg.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class dashboardg extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class dashboardg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboardg);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     public void joinroom(View view) {
