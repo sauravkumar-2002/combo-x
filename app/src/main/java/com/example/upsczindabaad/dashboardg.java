b488c2e6e9954b515100905275b614f4884634a4
Sandeep-0404 <sandeepgh2002@gmail.com>
2021-07-24 20:37:50 +0530

    animate

--- app/src/main/java/com/example/upsczindabaad/dashboardg.java
+++ app/src/main/java/com/example/upsczindabaad/dashboardg.java
@@ -1,13 +1,22 @@
 package com.example.upsczindabaad;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class dashboardg extends AppCompatActivity {
+
+    ImageView mainImage;
+    TextView icText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +24,25 @@ public class dashboardg extends AppCompatActivity {
         setContentView(R.layout.activity_dashboardg);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mainImage=findViewById(R.id.mainUserIMage);
+        icText=findViewById(R.id.individualChatText);
     }
 
     public void joinroom(View view) {
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void startchat(View view) {
         Intent intent1=new Intent(getApplicationContext(),individualchat.class);
-        startActivity(intent1);
+        Pair[] pairs=new Pair[1];
+        pairs[0]=new Pair<View,String>(mainImage,"IndividualChatsImage");
+
+
+        ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(this,pairs);
+
+
+        startActivity(intent1,options.toBundle());
     }
 
     public void createroom(View view) {
