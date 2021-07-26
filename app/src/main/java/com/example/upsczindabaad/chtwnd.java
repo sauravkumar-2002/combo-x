d787dc55df7b1bad226bcda0192ef388b49d4090
Saurav Kumar <https://github.com/sauravkumar-2002/Health-Companion>
2021-07-26 12:13:51 +0530

    rcv chat

--- app/src/main/java/com/example/upsczindabaad/chtwnd.java
+++ app/src/main/java/com/example/upsczindabaad/chtwnd.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,6 +20,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 public class chtwnd extends AppCompatActivity {
 
@@ -27,7 +32,10 @@ public class chtwnd extends AppCompatActivity {
     String chatPerson;
     String userNameForGrp;
     userdatamodel umd4;
-
+    RecyclerView chatrecv;
+    ic_adapter ic_adapter;
+    ArrayList<ic_model1>list;
+    String nameown,msges,chetperson;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +46,8 @@ public class chtwnd extends AppCompatActivity {
         chatPersonName = findViewById(R.id.chatPersonName);
         chatPersonImage = findViewById(R.id.chatPersonImage);
         emojiImage=findViewById(R.id.emojiImage);
+        chatrecv=findViewById(R.id.chatrecv);
+        chatrecv.setLayoutManager(new LinearLayoutManager(this));
         edtmsg = findViewById(R.id.edtmsg);
         Intent intent = getIntent();
         chatPerson = intent.getStringExtra("name");
@@ -45,6 +55,9 @@ public class chtwnd extends AppCompatActivity {
         chatPersonName.setText(chatPerson);
         chatPersonImage.setImageResource(chatPersonImages);
 
+        list=new ArrayList<>();
+        ic_adapter=new ic_adapter(list,this);
+        chatrecv.setAdapter(ic_adapter);
 
     }
 
@@ -80,6 +93,12 @@ public class chtwnd extends AppCompatActivity {
                 ref1.push().setValue(ch);
                 ref2.push().setValue(ch2);
                 edtmsg.setText("");
+
+
+
+                retrievedataforrecyclerview();
+
+
             }
 
             @Override
@@ -87,6 +106,74 @@ public class chtwnd extends AppCompatActivity {
 
             }
         });
+
+
+    }
+
+    private void retrievedataforrecyclerview() {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("List of Users").child(uid);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                umd4 = new userdatamodel();
+                umd4 = snapshot.getValue(userdatamodel.class);
+                nameown= umd4.getUsername();
+                chetperson=nameown+chatPerson;
+                FirebaseDatabase dbs=FirebaseDatabase.getInstance();
+                DatabaseReference reference=dbs.getReference("private chats").child(chetperson);
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        list.clear();
+                        for(DataSnapshot s:snapshot.getChildren()){
+                            chatwndmodel chatwndmodel=s.getValue(com.example.upsczindabaad.chatwndmodel.class);
+                            String messagesnapshot=chatwndmodel.getMsg();
+                            String frontuseruid=chatwndmodel.getUid();
+                            FirebaseDatabase dbsname=FirebaseDatabase.getInstance();
+                            DatabaseReference reference1=dbsname.getReference("List of Users").child(frontuseruid);
+                            reference1.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    userdatamodel frontusernamemodel=snapshot.getValue(userdatamodel.class);
+                                    String frontusername=frontusernamemodel.getUsername();
+
+
+
+                                    ic_model1 ic_model1=new ic_model1();
+                                    ic_model1.setName(frontusername);
+                                    ic_model1.setMsg(messagesnapshot);
+                                    list.add(ic_model1);
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+
+                         ic_adapter.notifyDataSetChanged();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
 
 
     }
