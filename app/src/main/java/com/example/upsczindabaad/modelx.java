f047b8731c20f349cd19c37b3dec2b6a1773f3c8
Saurav Kumar <https://github.com/sauravkumar-2002/Health-Companion>
2021-07-29 21:07:27 +0530

    totally demoralised

---
+++ app/src/main/java/com/example/upsczindabaad/modelx.java
@@ -0,0 +1,13 @@
+package com.example.upsczindabaad;
+
+public class modelx {
+    String invitecode;
+
+    public String getInvitecode() {
+        return invitecode;
+    }
+
+    public void setInvitecode(String invitecode) {
+        this.invitecode = invitecode;
+    }
+}
